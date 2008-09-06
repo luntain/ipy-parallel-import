a5afd14d2b9c72d5c7cf4b170410c6ae34e65835
U-ORGANON2\Luntain <kamil@dworakowski.name>
2008-09-06 22:12:46 +0100

    wow! that could actually work in Resolver One :)

--- UnitTests/preimport_test.py
+++ UnitTests/preimport_test.py
@@ -1,5 +1,5 @@
 from unittest import TestCase, TestLoader, TextTestRunner
-from preimport import preimport
+from preimport import preimport, ImportTaskMaster
 from testdata.importgraph import import_graph
 import sys
 
@@ -17,6 +17,35 @@ class preimport_test(TestCase):
         preimport(import_graph)
         for earthling in nodes:
             assert earthling in sys.modules
+
+
+class ImportTaskMasterTest(TestCase):
+
+    def test_empty_graph(self):
+        tm = ImportTaskMaster({})
+        self.assertEquals(tm.next(), '!all_done!')
+        self.assertEquals(tm.next(), '!all_done!')
+
+    def test_simple_dependency(self):
+        tm = ImportTaskMaster({'bear': set(['seal'])})
+        self.assertEquals([tm.next(), tm.next(), tm.next()], ['seal', None, None])
+        tm.done('seal')
+        self.assertEquals([tm.next(), tm.next()], ['bear', '!all_done!'])
+
+    def test_more_involved_case(self):
+        tm = ImportTaskMaster({
+            'human': set(['bear']),
+            'bear': set(['seal', 'honey'])})
+
+        self.assertEquals(set([tm.next(), tm.next()]), set(['seal', 'honey']))
+        self.assertEquals(tm.next(), None)
+        tm.done('seal')
+        self.assertEquals(tm.next(), None)
+        tm.done('honey')
+        self.assertEquals([tm.next(), tm.next()], ['bear', None])
+        tm.done('bear')
+        self.assertEquals([tm.next(), tm.next()], ['human', '!all_done!'])
+
 
 
 if __name__ == '__main__':
--- preimport.py
+++ preimport.py
@@ -1,8 +1,75 @@
 import sys
 
-def preimport(graph):
-    for key, value in graph.items():
-        __import__(key)
-        for module in value:
-            __import__(module)
+from System.Threading import Monitor, Thread, ThreadStart
+
+class ImportTaskMaster(object):
+    def __init__(self, graph):
+        self.mod2dep = graph
+        self.leaves = set()
+        for module in self.all_modules():
+            if module not in self.mod2dep or not self.mod2dep[module]:
+                self.leaves.add(module)
+        self.lock = object()
+
+    def all_modules(self):
+        for module, dependencies in self.mod2dep.iteritems():
+            for dep in dependencies:
+                yield dep
+            yield module
+
+    def next(self):
+        Monitor.Enter(self.lock)
+        try:
+            if self.leaves:
+                module = self.leaves.pop()
+                if module in self.mod2dep:
+                    del self.mod2dep[module]
+                return module
+            else:
+                if self.mod2dep:
+                    return None
+                else:
+                    return '!all_done!'
+        finally:
+            Monitor.Exit(self.lock)
+
+    def done(self, imported):
+        Monitor.Enter(self.lock)
+        try:
+            for module, deps in self.mod2dep.items():
+                if module == imported:
+                    del self.mod2dep[imported]
+                else:
+                    if imported in deps:
+                        deps.remove(imported)
+                    if not deps:
+                        self.leaves.add(module)
+        finally:
+            Monitor.Exit(self.lock)
+
+
+def preimport(graph, threads=4):
+
+    taskmaster = ImportTaskMaster(graph)
+
+    def worker():
+        while 1:
+            next_module = taskmaster.next()
+            if next_module == '!all_done!':
+                return
+            if next_module is None:
+                print 'no modules eligible to import, sleeping'
+                Thread.Sleep(200)
+            else:
+                __import__(next_module)
+                taskmaster.done(next_module)
+
+    workers = []
+    for _ in range(threads):
+        w = Thread(ThreadStart(worker))
+        w.Start()
+        workers.append(w)
+
+    for w in workers:
+        w.Join()
 
